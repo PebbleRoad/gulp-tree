515017f3f72e9cf907dae22f37154cdcfd1260fd
Vinay <rmdort@gmail.com>
2014-02-26 22:44:33 +0800

    Children filenames will be restored

---
+++ README.md
@@ -0,0 +1,50 @@
+# gulp-tree
+
+> Tree creates a structured hierarchical JSON from a folder structure
+
+## Usage
+
+First, install `gulp-tree` as a development dependency:
+
+```shell
+npm install --save-dev gulp-tree
+```
+
+Then, add it to your `gulpfile.js`:
+
+```javascript
+var tree = require("gulp-tree");
+
+gulp.src('./src/*.html')
+    .pipe(tree({
+        patternsPath: './src/patterns',
+        jsonPath: './src/json/'
+    }))
+```
+
+## API
+
+### tree(options)
+
+#### options.patternsPath
+Type: `String|Number`
+
+Default: `./src/patterns`
+
+Directory where the patterns are located
+
+#### options.jsonPath
+Type: `String`
+
+Default: './src/json'
+
+Directory where the json should be saved
+
+## Usage
+
+Tree creates a structured hierarchical JSON from a folder structure
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ index.js
@@ -0,0 +1,184 @@
+var fs, handle, path, tree, yaml, marked;
+
+fs = require('fs');
+yaml = require('js-yaml');
+marked = require('marked'); // markdown processor   
+path = require('path');
+var util = require('gulp-util');
+
+var through = require("through2");
+var es = require("event-stream");
+
+
+var generateTree = function(param){
+
+  'use strict'
+
+  var params = param || {},
+      patternsPath = path.resolve(params.patternsPath || './src/patterns'),
+      jsonPath = path.resolve(params.jsonPath || './src/json/')
+
+  var stream = through.obj(function (file, enc, callback) {
+
+      /* File passed in */
+      
+      var parsedFile = tree(patternsPath)
+
+
+      //var treeobject = eval("(" + parseFile + ")")
+      var s = parsedFile.children.filter(function(a, b){
+        return a.children
+      })
+
+      /**
+       * If patternsPath doesnt exist
+       */
+      
+      fs.stat(patternsPath, function(err, stats){
+        if(err && erro.errno == 34){
+          this.emit('error', new PluginError('gulp-tree', 'Patterns directory doesnt exist'));
+        }
+      })
+
+
+      /**
+       * Write the file
+       */
+      
+      fs.stat(jsonPath, function(err, stats){
+
+        if(err && err.errno == 34){
+
+          fs.mkdir(jsonPath)
+
+        }
+      })
+
+      var files = [];
+
+      for(var i = 0; i< s.length; i++){
+          
+          if(s[i].children && s[i].children.length){
+              
+              files.push(jsonPath+ '/' + s[i].slug + '.json')
+
+              fs.writeFile(jsonPath + '/'+ s[i].slug+".json", JSON.stringify(s[i].children, null, 4), function(err){
+                  if(err) {
+                      console.log(err);
+                  } else {
+                                          
+                      console.log("Generated json in " + jsonPath);
+                  }
+              })  
+          }
+          
+
+      }
+
+      return files;
+
+      
+      
+      
+
+  });
+
+  var excludedExtension = ['.png', '.jpg', '.git', '.jpeg', '.gif', '.html'];
+  var tree = function(root){
+      
+      var info, ring;
+      root = root.replace(/\/+$/, "");
+      if (fs.existsSync(root)) {
+          ring = fs.lstatSync(root);
+      } else {
+          return 'error: root does not exist';
+      }
+      info = {
+          path: root.replace(process.cwd()+'/src/', ''),
+          name: path.basename(root).replace(/^\d./, ''),
+          slug: path.basename(root)
+      };
+
+
+      if (ring.isDirectory()) {
+
+          var children = fs.readdirSync(root)         
+          
+          var filtered = children.filter(function(c){
+              
+              return c != '.DS_Store' && excludedExtension.indexOf(path.extname(c)) == -1
+
+          })
+
+          delete(info.path)
+          delete(info.type)
+
+          //info.type = 'folder';
+
+          info.children = filtered.map(function(child) {
+              return tree(root + '/' + child);
+          });
+      } else if (ring.isFile()) {
+
+          var contents = fs.readFileSync(root, {encoding: 'utf8'});
+          var extname = path.extname(root);
+          var parsedContent = {
+                      filename: root,
+                      filenameExtension: extname,
+                      yaml: '',
+                      markdown: '', 
+                      content: '',
+                      meta: {}
+                  };
+
+          var lines = contents.split('\n');   
+          
+          var frontMatter = '';
+
+          if (lines[0].trim() === '---' || lines[0].trim() == "") {
+              var firstFrontMatterMarker = lines.shift();
+              var line = '';
+              while (line !== '---') {
+                  frontMatter = frontMatter + line + "\n"; // since we split by \n we'll add it back here and stay true to the source doc
+                  line = lines.shift();
+              }
+          }
+          parsedContent.yaml = frontMatter;
+          parsedContent.meta = yaml.load(frontMatter);
+          
+          parsedContent.markdown = lines.join('\n');
+          parsedContent.content = parsedContent.markdown;
+
+          info.meta = {};
+          
+          for(var key in parsedContent.meta){
+            if(key != "name" && key != "description"){
+              if(parsedContent.meta.hasOwnProperty(key)){
+                info.meta[key] = parsedContent.meta[key]  
+              }              
+            }
+          }
+
+          if(Object.keys(info.meta).length < 1){
+            delete(info.meta)
+          }
+          
+          if(parsedContent.meta){
+              info.name = parsedContent.meta.name 
+          }
+          
+          
+      } else if (ring.isSymbolicLink()) {
+          info.type = 'link';
+      } else {
+          info.type = 'unknown';
+      }
+      return info;
+
+  }
+
+  return stream;
+
+}
+
+module.exports = generateTree;

a1aec780b3a847d53e9fa5ea0ab4815c2ebe66e4
Vinay <rmdort@gmail.com>
2014-03-01 16:51:09 +0800

    Better file handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs, handle, path, tree, yaml, marked;
 
 fs = require('fs');
-yaml = require('js-yaml');
+jsyaml = require('js-yaml');
 marked = require('marked'); // markdown processor   
 path = require('path');
 var util = require('gulp-util');
@@ -106,69 +106,71 @@ var generateTree = function(param){
           
           var filtered = children.filter(function(c){
               
-              return c != '.DS_Store' && excludedExtension.indexOf(path.extname(c)) == -1
+              var filepath = root + '/' + c,
+                  isFile = fs.lstatSync(filepath).isFile(),
+                  content = true;
+
+              if(isFile && fs.readFileSync(filepath) == ""){
+                content = false
+              }
+              
+              return c != '.DS_Store' && excludedExtension.indexOf(path.extname(c)) == -1 && content
 
           })
 
-          delete(info.path)
-          delete(info.type)
-
-          //info.type = 'folder';
+          delete(info.path)          
 
           info.children = filtered.map(function(child) {
               return tree(root + '/' + child);
           });
+
+
       } else if (ring.isFile()) {
 
           var contents = fs.readFileSync(root, {encoding: 'utf8'});
           var extname = path.extname(root);
-          var parsedContent = {
-                      filename: root,
-                      filenameExtension: extname,
-                      yaml: '',
-                      markdown: '', 
-                      content: '',
-                      meta: {}
-                  };
-
-          var lines = contents.split('\n');   
-
-          if(lines.length > 1){
           
-              var frontMatter = '';
+          var re = /^(-{3}(?:\n|\r)([\w\W]+?)-{3})?([\w\W]*)*/
+                , results = re.exec(contents.trim())
+                , conf = {}
+                , yamlOrJson;                
 
-              if (lines[0].trim() === '---' || lines[0].trim() == "") {
-                  var firstFrontMatterMarker = lines.shift();
-                  var line = '';
-                  while (line !== '---') {
-                      frontMatter = frontMatter + line + "\n"; // since we split by \n we'll add it back here and stay true to the source doc
-                      line = lines.shift();
-                  }
-              }
-              parsedContent.yaml = frontMatter;
-              parsedContent.meta = yaml.load(frontMatter);
-              
-              parsedContent.markdown = lines.join('\n');
-              parsedContent.content = parsedContent.markdown;
-
-              info.meta = {};
-              
-              for(var key in parsedContent.meta){
-                if(key != "name" && key != "description"){
-                  if(parsedContent.meta.hasOwnProperty(key)){
-                    info.meta[key] = parsedContent.meta[key]  
-                  }              
-                }
-              }
-
-              if(Object.keys(info.meta).length < 1){
-                delete(info.meta)
-              }
-              
-              if(parsedContent.meta){
-                  info.name = parsedContent.meta.name 
-              }
+          if((yamlOrJson = results[2])) {
+            
+            if(yamlOrJson.charAt(0) === '{') { 
+              conf = JSON.parse(yamlOrJson);
+            } else {
+              conf = jsyaml.load(yamlOrJson);
             }
+          }
+
+          if(conf.name){
+
+              info.name = conf.name
+          }else{
+              
+              return true;
+          }
+
+
+          /**
+           * Add meta variables
+           */
+          
+          info.meta = {};
+          
+          for(var key in conf){
+              if(key != "name" && key != "description"){
+              if(conf.hasOwnProperty(key)){
+                info.meta[key] = conf[key]  
+              }              
+            }
+          }
+
+          if(Object.keys(info.meta).length < 1){
+            delete(info.meta)
+          }
+          
           
       } else if (ring.isSymbolicLink()) {
           info.type = 'link';
